a326e87dc710dcba1f03c8cf62bf0b495809a70a
Andrew Breakspear <andrew@longhanborough.com>
2013-01-11 11:44:45 +0000

    Header now displayed by default

--- report.php
+++ report.php
@@ -5,19 +5,17 @@ require_once('engine/fpdf/fpdf.php');
 $pdf = new FPDF();
 $pdf->AddPage();
 
-if (isset($_GET['header'])) {
-	if ($_GET['header'] == 'true') {
-		include_once("modules/reports/views/header.php");
-	}
+// only exclude the header if explicitly asked to do so
+if ($_GET['header'] != 'false') {
+	include_once("modules/reports/views/header.php");
 }
 
+// include the node for the current report
 if (isset($_GET['n'])) {
 	$fileInclude = "modules/reports/nodes/" . $_GET['n'];
 	
 	include_once($fileInclude);
 }
-
-
 
 // Output the PDF
 $pdf->Output();
