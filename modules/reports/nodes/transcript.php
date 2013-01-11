94fa1861a22a417b8503ff15caec4a83b91bf66e
Andrew Breakspear <andrew@longhanborough.com>
2013-01-11 11:44:32 +0000

    Transcript now has option to exclude exam details

--- modules/reports/nodes/transcript.php
+++ modules/reports/nodes/transcript.php
@@ -30,43 +30,43 @@ $pdf->SetFont("Times", 'B', 12);
 $pdf->Cell(40, 10, $subject->name, 0, 0);
 $pdf->Ln();
 
-$pdf->SetFont("Times", 'B', 14);
-$pdf->Cell(40, 10, "Course Exams", 0, 1);
-
-$pdf->SetFont("Times", 'B', 10);
-$pdf->Cell(30, 7, "Date", 0, 0, 'C');
-$pdf->Cell(130, 7, "Name", 0, 0, 'L');
-$pdf->Cell(20, 7, "Result", 0, 0, 'C');
-$pdf->Ln();
-
-
-foreach ($exams AS $exam) {
-	$papers = $exam->find_papers_by_examkey($exam->examid);
-	$pdf->SetFont("Times", '', 10);
-	$pdf->Cell(30, 7, date("Y M", strtotime($exam->dt_exam)), 'T', 0, 'C');
-	$pdf->Cell(130, 7, $exam->name, 'T', 0, 'L');
-	$pdf->Cell(20, 7, $exam->qualname, 'T', 0, 'C');
-	$pdf->Ln();
+if ($_GET['exams'] != 'false') {
+	$pdf->SetFont("Times", 'B', 14);
+	$pdf->Cell(40, 10, "Course Exams", 0, 1);
 	
 	$pdf->SetFont("Times", 'B', 10);
-	$pdf->Cell(30, 7, "", 0, 0, 'C');
-	$pdf->Cell(20, 7, "Paper No", 'B', 0, 'C');
-	$pdf->Cell(90, 7, "Title", 'B', 0, 'L');
-	$pdf->Cell(20, 7, "Grade", 'B', 0, 'C');
-	$pdf->Cell(20, 7, "", 0, 0, 'C');
+	$pdf->Cell(30, 7, "Date", 0, 0, 'C');
+	$pdf->Cell(130, 7, "Name", 0, 0, 'L');
+	$pdf->Cell(20, 7, "Result", 0, 0, 'C');
 	$pdf->Ln();
 	
-	$pdf->SetFont("Times", '', 10);
-	foreach ($papers AS $paper) {
+	foreach ($exams AS $exam) {
+		$papers = $exam->find_papers_by_examkey($exam->examid);
+		$pdf->SetFont("Times", '', 10);
+		$pdf->Cell(30, 7, date("Y M", strtotime($exam->dt_exam)), 'T', 0, 'C');
+		$pdf->Cell(130, 7, $exam->name, 'T', 0, 'L');
+		$pdf->Cell(20, 7, $exam->qualname, 'T', 0, 'C');
+		$pdf->Ln();
+		
+		$pdf->SetFont("Times", 'B', 10);
 		$pdf->Cell(30, 7, "", 0, 0, 'C');
-		$pdf->Cell(20, 7, $paper->paperno, 0, 0, 'C');
-		$pdf->Cell(90, 7, $paper->title, 0, 0, 'L');
-		$pdf->Cell(20, 7, $paper->grade, 0, 0, 'C');
+		$pdf->Cell(20, 7, "Paper No", 'B', 0, 'C');
+		$pdf->Cell(90, 7, "Title", 'B', 0, 'L');
+		$pdf->Cell(20, 7, "Grade", 'B', 0, 'C');
 		$pdf->Cell(20, 7, "", 0, 0, 'C');
 		$pdf->Ln();
+		
+		$pdf->SetFont("Times", '', 10);
+		foreach ($papers AS $paper) {
+			$pdf->Cell(30, 7, "", 0, 0, 'C');
+			$pdf->Cell(20, 7, $paper->paperno, 0, 0, 'C');
+			$pdf->Cell(90, 7, $paper->title, 0, 0, 'L');
+			$pdf->Cell(20, 7, $paper->grade, 0, 0, 'C');
+			$pdf->Cell(20, 7, "", 0, 0, 'C');
+			$pdf->Ln();
+		}
 	}
 }
-
 $pdf->Ln(20);
 $pdf->Cell(0, 10, "Signed:", 0, 1);
 $pdf->Ln(10);
