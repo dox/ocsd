f4a90436e1f634b77cd3686153a8810cc545a870
Andrew Breakspear <andrew@longhanborough.com>
2013-03-04 12:26:52 +0000

    Tabs now replace, instead of append
    Fixes #3

--- js/ocsd.js
+++ js/ocsd.js
@@ -1,17 +1,15 @@
-function show_tab(tab) {
-  if (!tab.html()) {
-    tab.load(tab.attr('data-target'));
-  }
-}
-
-function init_tabs() {
-  show_tab($('.tab-pane.active'));
-  $('a[data-toggle="tab"]').on('show', function(e) {
-    tab = $('#' + $(e.target).attr('href').substr(1));
-    show_tab(tab);
-  });
-}
-
-$(function () {
-  init_tabs();
+$(function() {
+	$("#MainTabs").tab();
+	
+	$("#MainTabs").bind("show", function(e) {    
+		var contentID  = $(e.target).attr("data-target");
+		var contentURL = $(e.target).attr("href");
+		
+		if (typeof(contentURL) != 'undefined')
+			$(contentID).load(contentURL, function(){ $("#MainTabs").tab(); });
+		else
+			$(contentID).tab('show');
+	});
+	
+	$('#MainTabs a:first').tab("show");
 });
--- modules/students/nodes/index.php
+++ modules/students/nodes/index.php
@@ -5,16 +5,16 @@
 		</div>
 		<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
 		
-		<div class="tabbable"> <!-- Only required for left/right tabs -->
-			<ul class="nav nav-tabs" id="ajax_tabs">
-				<li class="active"><a data-toggle="tab" href="#ajax_students">Students</a></li>
-				<li><a data-toggle="tab" href="#ajax_tutors">Tutors</a></li>
-				<li><a data-toggle="tab" href="#ajax_archive">Archive</a></li>
-			</ul>
-			
-			<div class="tab-pane active" id="ajax_students" data-target="modules/students/nodes/tab_students.php"></div>
-			<div class="tab-pane" id="ajax_tutors" data-target="modules/students/nodes/tab_tutors.php"></div>
-			<div class="tab-pane" id="ajax_archive" data-target="modules/students/nodes/tab_archive.php"></div>
+		<ul class="nav nav-tabs" id="MainTabs">
+			<li><a data-target="#ajax_students" data-toggle="tab" href="modules/students/nodes/tab_students.php">Students</a></li>
+			<li><a data-target="#ajax_tutors" data-toggle="tab" href="modules/students/nodes/tab_tutors.php">Tutors</a></li>
+			<li><a data-target="#ajax_archive" data-toggle="tab" href="modules/students/nodes/tab_archive.php">Archive</a></li>
+		</ul>
+		
+		<div class="tab-content">
+			<div class="tab-pane" id="ajax_students"><i class="icon-refresh"></i> Loading...</div>
+			<div class="tab-pane" id="ajax_tutors"><i class="icon-refresh"></i> Loading...</div>
+			<div class="tab-pane" id="ajax_archive"><i class="icon-refresh"></i> Loading...</div>
 		</div>
 		
 		<p><a class="btn" href="#">View details &raquo;</a></p>
