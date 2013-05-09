73fc8537f5624fb2fd74fa51e370e67cd6b51f9a
Andrew Breakspear <andrew@longhanborough.com>
2013-05-09 15:48:23 +0100

    Logon now takes place without blank screen
    Logging on now works without requiring a page refresh.
    Fixes #25

--- index.php
+++ index.php
@@ -1,5 +1,60 @@
 <?php
 include_once("engine/initialise.php");
+
+//log them out?
+if (isset($_GET['logout'])) {
+	if ($_GET['logout'] == "true") { //destroy the session
+		$_SESSION = array();
+		session_destroy();
+		
+		$message = "<div class=\"alert alert-success\"><button type=\"button\" class=\"close\" data-dismiss=\"alert\">×</button><strong>Success!</strong> You have been logged out.</div>";
+	}
+}
+
+//you should look into using PECL filter or some form of filtering here for POST variables
+if (isset($_POST["username"])) {
+	$username = strtoupper($_POST["username"]); //remove case sensitivity on the username
+	$password = $_POST["password"];
+}
+
+if (isset($_POST["oldform"])) { //prevent null bind
+
+	if ($username != NULL && $password != NULL){
+        try {
+		    $adldap = new adLDAP();
+        }
+        catch (adLDAPException $e) {
+            echo $e; 
+            exit();   
+        }
+		
+		//authenticate the user
+		if ($adldap->authenticate($username, $password)){
+			//establish your session and redirect
+			session_start();
+			$_SESSION["username"] = $username;
+            $_SESSION["userinfo"] = $adldap->user()->info($username);
+			$redir = "Location: http://" . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . "/index.php";
+			
+			$log = new Logs;
+			$log->notes			= $_SESSION["username"] . " logged on";
+			$log->type			= "logon";
+			$log->create();
+			
+			header($redir);
+			exit;
+		} else {
+			$log = new Logs;
+			$log->notes			= "Unsuccessful logon attempt";
+			$log->prev_value	= $username;
+			$log->type			= "logon";
+			$log->create();
+		}
+	}
+	
+	$message = "<div class=\"alert\"><button type=\"button\" class=\"close\" data-dismiss=\"alert\">×</button><strong>Warning!</strong> Login attempt failed.</div>";
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -8,26 +63,29 @@ include_once("engine/initialise.php");
 
 <body>
 	<?php
+	if (isset($message)) {
+		echo $message;
+	}
+	
+	$fileInclude = "nodes/logon.php";
+	if (isset($_SESSION['username'])) {
+		// we're logged in, work out what to include
+		if (isset($_GET['m'])) {
+			$fileInclude = "modules/" . $_GET['m'] . "/nodes/" . $_GET['n'];
+		} elseif(isset($_GET['n'])) {
+			$fileInclude = "nodes/" . $_GET['n'];
+		} else {
+			$fileInclude = "nodes/index.php";
+		}
+	} else {
+		$fileInclude = "nodes/logon.php";
+	}
+	
 	include_once("views/navigation.php");
 	include_once("views/hero.php");
 	?>
 	<div class="container">
 		<?php
-		//echo "U: " . $_SESSION['username'];
-		$fileInclude = "nodes/logon.php";
-		if (isset($_SESSION['username'])) {
-			// we're logged in, work out what to include
-			if (isset($_GET['m'])) {
-				$fileInclude = "modules/" . $_GET['m'] . "/nodes/" . $_GET['n'];
-			} elseif(isset($_GET['n'])) {
-				$fileInclude = "nodes/" . $_GET['n'];
-			} else {
-				$fileInclude = "nodes/index.php";
-			}
-		} else {
-			$fileInclude = "nodes/logon.php";
-		}
-		
 		include_once($fileInclude);
 		?>
 		<?php include_once("views/footer.php"); ?>
--- nodes/logon.php
+++ nodes/logon.php
@@ -1,58 +1,3 @@
-<?php
-//log them out?
-if (isset($_GET['logout'])) {
-	if ($_GET['logout'] == "true") { //destroy the session
-		$_SESSION = array();
-		session_destroy();
-		
-		$message = "<div class=\"alert alert-success\"><button type=\"button\" class=\"close\" data-dismiss=\"alert\">×</button><strong>Success!</strong> You have been logged out.</div>";
-	}
-}
-
-//you should look into using PECL filter or some form of filtering here for POST variables
-if (isset($_POST["username"])) {
-	$username = strtoupper($_POST["username"]); //remove case sensitivity on the username
-	$password = $_POST["password"];
-}
-
-if (isset($_POST["oldform"])) { //prevent null bind
-
-	if ($username != NULL && $password != NULL){
-        try {
-		    $adldap = new adLDAP();
-        }
-        catch (adLDAPException $e) {
-            echo $e; 
-            exit();   
-        }
-		
-		//authenticate the user
-		if ($adldap->authenticate($username, $password)){
-			//establish your session and redirect
-			session_start();
-			$_SESSION["username"] = $username;
-            $_SESSION["userinfo"] = $adldap->user()->info($username);
-			$redir = "Location: http://" . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . "/index.php";
-			
-			$log = new Logs;
-			$log->notes			= $_SESSION["username"] . " logged on";
-			$log->type			= "logon";
-			$log->create();
-			
-			header($redir);
-			exit;
-		} else {
-			$log = new Logs;
-			$log->notes			= "Unsuccessful logon attempt";
-			$log->prev_value	= $username;
-			$log->type			= "logon";
-			$log->create();
-		}
-	}
-	
-	$message = "<div class=\"alert\"><button type=\"button\" class=\"close\" data-dismiss=\"alert\">×</button><strong>Warning!</strong> Login attempt failed.</div>";
-}
-?>
 <style type="text/css">
 .form-signin {
 	max-width: 300px;
@@ -82,14 +27,8 @@ if (isset($_POST["oldform"])) { //prevent null bind
 
 <div class="row">
 	<div class="container">
-		<form class="form-signin" id="loginForm" action="index.php?m=students&n=index.php" method="post">
+		<form class="form-signin" id="loginForm" method="post">
 		<h2 class="form-signin-heading">Logon Required</h2>
-		
-		<?php
-		if (isset($message)) {
-			echo $message;
-		}
-		?>
 		
 		<input type="text" class="input-block-level" placeholder="Username" id="username" name="username" value="<?php if (isset($_POST['username'])) { echo $username; } ?>">
 		<input type="password" class="input-block-level" placeholder="Password" name="password" id="password">
