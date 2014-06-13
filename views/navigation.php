d38425beedc2e7db20a55ba073848d459a953bb5
Andrew Breakspear <andrew@spy.seh.ox.ac.uk>
2014-06-13 14:36:25 +0100

    Fixing quick search
    Quick search (in the navigation) now works with the new
    bootstrap/typeahead and the new Bllodhound search library/handlebars,
    instead of old typeahead/hogan.

--- views/navigation.php
+++ views/navigation.php
@@ -1,4 +1,5 @@
 <script src="js/typeahead.jquery.min.js"></script>
+<script src="js/handlebars.js"></script>
 <script src="js/bloodhound.min.js"></script>
 
 <div class="navbar navbar-default navbar-fixed-top" role="navigation">
@@ -30,7 +31,6 @@
 					?>
 					</div>
 				</div>
-				<button type="submit" class="btn btn-default">SEARCH NOT WORKING</button>
 			</form>
 			
 			<ul class="nav navbar-nav navbar-right">
@@ -53,100 +53,76 @@
 </div>
 
 <style>
-.twitter-typeahead .tt-query,
-.twitter-typeahead .tt-hint {
-	margin-bottom: 0;
+.tt-query,
+.tt-hint {
+    width: 396px;
+    height: 30px;
+    padding: 8px 12px;
+    font-size: 24px;
+    line-height: 30px;
+    border: 2px solid #ccc;
+    border-radius: 8px;
+    outline: none;
+}
+
+.tt-query {
+    box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
+}
+
+.tt-hint {
+    color: #999
 }
 
 .tt-dropdown-menu {
-	min-width: 160px;
-	margin-top: 2px;
-	padding: 5px 0;
-	background-color: #fff;
-	border: 1px solid #ccc;
-	border: 1px solid rgba(0,0,0,.2);
-	*border-right-width: 2px;
-	*border-bottom-width: 2px;
-	-webkit-border-radius: 6px;
-	-moz-border-radius: 6px;
-	border-radius: 6px;
-	-webkit-box-shadow: 0 5px 10px rgba(0,0,0,.2);
-	-moz-box-shadow: 0 5px 10px rgba(0,0,0,.2);
-	box-shadow: 0 5px 10px rgba(0,0,0,.2);
-	-webkit-background-clip: padding-box;
-	-moz-background-clip: padding;
-	background-clip: padding-box;
+    width: 422px;
+    margin-top: 12px;
+    padding: 8px 0;
+    background-color: #fff;
+    border: 1px solid #ccc;
+    border: 1px solid rgba(0, 0, 0, 0.2);
+    border-radius: 8px;
+    box-shadow: 0 5px 10px rgba(0,0,0,.2);
 }
 
 .tt-suggestion {
-	display: block;
-	padding: 3px 20px;
+    padding: 3px 20px;
+    font-size: 18px;
+    line-height: 24px;
 }
 
-.tt-suggestion.tt-is-under-cursor {
-	color: #fff;
-	background-color: #0081c2;
-	background-image: -moz-linear-gradient(top, #0088cc, #0077b3);
-	background-image: -webkit-gradient(linear, 0 0, 0 100%, from(#0088cc), to(#0077b3));
-	background-image: -webkit-linear-gradient(top, #0088cc, #0077b3);
-	background-image: -o-linear-gradient(top, #0088cc, #0077b3);
-	background-image: linear-gradient(to bottom, #0088cc, #0077b3);
-	background-repeat: repeat-x;
-	filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#ff0088cc', endColorstr='#ff0077b3', GradientType=0)
-}
+.tt-suggestion.tt-is-under-cursor { /* UPDATE: newer versions use .tt-suggestion.tt-cursor */
+    color: #fff;
+    background-color: #0097cf;
 
-.tt-suggestion.tt-is-under-cursor a {
-	color: #fff;
 }
 
 .tt-suggestion p {
-	margin: 0;
-}
-</style>
+    margin: 0;
+}</style>
 
 <script>
-
 var bestPictures = new Bloodhound({
-  datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
-  queryTokenizer: Bloodhound.tokenizers.whitespace,
-  prefetch: './api/studentsAll.php',
-  remote: './api/studentsAll.php'
+	datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
+	queryTokenizer: Bloodhound.tokenizers.whitespace,
+	prefetch: './api/studentsAll.php'
 });
- 
+
 bestPictures.initialize();
  
 $('#nav-search .typeahead').typeahead(null, {
 	hint: true,
 	highlight: true,
-//	minLength: 1,
+	minLength: 1,
 	name: 'users-current',
-	displayKey: 'name',
+	displayKey: 'value',
 	source: bestPictures.ttAdapter(),
 	templates: {
 		empty: [
 			'<div class="empty-message">',
-			'unable to find any Best Picture winners that match the current query',
+			'<p>No students found</p>',
 			'</div>'
-		].join('\n')
+		].join('\n'),
+		suggestion: Handlebars.compile('<p><a href="index.php?m=students&n=user.php&studentid={{value}}">{{name}}</a></p>')
 	}
 });
-
-
-
-
-/*
-$('.typeahead').typeahead({
-	name: 'twitter',
-	prefetch: './api/studentsAll.php',
-	template: '<p class="repo-language">{{name}}</p>',
-	engine: Hogan
-}).on('typeahead:selected', function($e) {
-	var $typeahead = $(this);
-	var studentUID = $typeahead[0].value;
-	var url = 'index.php?m=students&n=user.php&studentid=' + studentUID;
-	
-	window.location = url;
-
-});
-*/
 </script>
