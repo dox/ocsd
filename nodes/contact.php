19b33a722b786b59c0307371bd12567514369f23
Andrew Breakspear <andrew.breakspear@seh.ox.ac.uk>
2014-06-16 10:46:52 +0100

    Updating contact (and nav flag) to be BS3 pretty

--- nodes/contact.php
+++ nodes/contact.php
@@ -1,56 +1,52 @@
-<div class="row">
-	<div class="span12">
-		<div class="page-header">
-			<h1>Contact <small> please submit your query</small></h1>
-		</div>
-	</div>
-	<div class="span12">
-		<form class="form-horizontal" id="contactForm">
-		<div class="control-group">
-			<label class="control-label" for="inputEmail">Your E-Mail</label>
-			<div class="controls">
-				<input type="text" id="inputEmail" placeholder="E-Mail">
-			</div>
-		</div>
-		<div class="control-group">
-			<label class="control-label" for="inputStudent">Reference Student</label>
-			<div class="controls">
-				<?php
-				if (isset($_GET['studentName'])) {
-					$value = $_GET['studentName'];
-				} else {
-					$value = "";
-				}
-				?>
-				<input type="text" id="inputStudent" placeholder="Full Student Name" value="<?php echo $value; ?>">
-			</div>
-		</div>
-		<?php
-		if (isset($_GET['studentID'])) {
-		?>
-		<div class="control-group">
-			<label class="control-label" for="inputStudentID">Reference Student ID</label>
-			<div class="controls">
-				<input type="text" id="inputStudentID" placeholder="Student ID" value="<?php echo $_GET['studentID']; ?>">
-			</div>
-		</div>
-		<?php
-		}
-		?>
-		<div class="control-group">
-			<label class="control-label" for="textareaMessage">Query/Problem</label>
-			<div class="controls">
-				<textarea rows="3" id="textareaMessage" autofocus></textarea>
-			</div>
-		</div>
-		<div class="control-group">
-			<div class="controls">
-				<button type="button" class="btn btn-primary" id="submitFormButton">Submit</button>
-			</div>
-		</div>
-		</form>
+<div class="page-header">
+	<h1>Contact <small> please submit your query</small></h1>
+</div>
+
+<form class="form-horizontal" id="contactForm" role="form">
+<div class="form-group">
+	<label class="col-sm-2 control-label" for="inputEmail">Your E-Mail</label>
+	<div class="col-sm-10">
+		<input type="email" class="form-control" id="inputEmail" placeholder="E-Mail">
 	</div>
 </div>
+<div class="form-group">
+	<label class="col-sm-2 control-label" for="inputStudent">Reference Student</label>
+	<div class="col-sm-10">
+		<?php
+		if (isset($_GET['studentName'])) {
+			$value = $_GET['studentName'];
+		} else {
+			$value = "";
+		}
+		?>
+		<input type="text" id="inputStudent" placeholder="Full Student Name" value="<?php echo $value; ?>">
+	</div>
+</div>
+
+<?php
+if (isset($_GET['studentID'])) {
+?>
+<div class="form-group">
+		<label class="col-sm-2 control-label" for="inputStudentID">Reference Student ID</label>
+		<div class="col-sm-10">
+			<input type="number" id="inputStudentID" placeholder="Student ID" value="<?php echo $_GET['studentID']; ?>">
+		</div>
+	</div>
+<?php
+}
+?>
+<div class="form-group">
+	<label class="col-sm-2 control-label" for="textareaMessage">Query/Problem</label>
+	<div class="col-sm-10">
+		<textarea class="form-control" rows="3" id="textareaMessage" autofocus></textarea>
+	</div>
+</div>
+<div class="form-group">
+	<div class="col-sm-offset-2 col-sm-10">
+		<button type="button" class="btn btn-primary" id="submitFormButton">Submit</button>
+	</div>
+</div>
+</form>
 
 <script>
 $(function() {
--- views/navigation.php
+++ views/navigation.php
@@ -34,7 +34,7 @@
 			</form>
 			
 			<ul class="nav navbar-nav navbar-right">
-				<li><a href="index.php?n=contact.php" class="btn btn-primary btn-mini pull-right"><i class="fa fa-flag"></i></a></li>
+				<li><a href="index.php?n=contact.php"><i class="fa fa-flag"></i></a></li>
 				<li class="dropdown">
 				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Admin <b class="caret"></b></a>
 					<ul class="dropdown-menu">
