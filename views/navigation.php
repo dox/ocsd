40e393ac98fe91a0ec14fa63da38429a5fdf9110
Andrew Breakspear <andrew.breakspear@seh.ox.ac.uk>
2014-06-16 08:55:39 +0100

    Making navbar not a fluid container

--- views/navigation.php
+++ views/navigation.php
@@ -3,7 +3,7 @@
 <script src="js/bloodhound.min.js"></script>
 
 <div class="navbar navbar-default navbar-fixed-top" role="navigation">
-	<div class="container-fluid">
+	<div class="container">
 		<div class="navbar-header">
 			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
 				<span class="sr-only">Toggle navigation</span>
