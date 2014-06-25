e1568feb2d49c35185326c4d035979d3ba6c0c94
Andrew Breakspear <andrew@spy.seh.ox.ac.uk>
2014-06-25 11:30:08 +0100

    Tidying up the transcript button for BS3

--- modules/students/nodes/user.php
+++ modules/students/nodes/user.php
@@ -276,18 +276,13 @@ $studentAwards = student_awardsClass::find_by_studentkey($user->id());
 			<div class="tab-pane" id="reports">
 				<p>
 				<div class="btn-group">
-					<?php
-					echo "<a href=\"report_pdf.php?n=transcript.php&studentid=" . $user->id() . "\" class=\"btn\">Generate Transcript</a>";
-					?>
-					<button class="btn dropdown-toggle" data-toggle="dropdown"><span class="caret"></span></button>
-					<ul class="dropdown-menu">
-						<li class="nav-header">With Letterhead</li>
-						<li><a href="report_pdf.php?n=transcript.php&studentid=<?php echo $user->id(); ?>">With exam paper details</a></li>
-						<li><a href="report_pdf.php?n=transcript.php&exams=false&studentid=<?php echo $user->id(); ?>">Without exam paper details</a></li>
-						
-						<li class="nav-header">Without Letterhead</li>
-						<li><a href="report_pdf.php?n=transcript.php&studentid=<?php echo $user->id(); ?>&header=false">With exam paper details</a></li>
-						<li><a href="report_pdf.php?n=transcript.php&exams=false&studentid=<?php echo $user->id(); ?>&header=false">Without exam paper details</a></li>
+					<button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown">Generate Transcript <span class="caret"></span></button>
+					<ul class="dropdown-menu" role="menu">
+						<li><a href="report_pdf.php?n=transcript.php&studentid=<?php echo $user->id(); ?>">Letterhead and exam paper details</a></li>
+						<li><a href="report_pdf.php?n=transcript.php&exams=false&studentid=<?php echo $user->id(); ?>">Letterhead without exam paper details</a></li>
+						<li class="divider"></li>
+						<li><a href="report_pdf.php?n=transcript.php&studentid=<?php echo $user->id(); ?>&header=false">Without letterhead and with exam paper details</a></li>
+						<li><a href="report_pdf.php?n=transcript.php&exams=false&studentid=<?php echo $user->id(); ?>&header=false">Without letterhead and without exam paper details</a></li>
 					</ul>
 				</div>
 				</p>
