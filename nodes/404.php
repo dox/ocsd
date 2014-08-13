fe3bca28ed5c5cce8279084dc9a74ee91381f6ce
Andrew Breakspear <andrew@longhanborough.com>
2014-08-13 13:45:05 +0100

    404 now prettier

--- nodes/404.php
+++ nodes/404.php
@@ -1,7 +1,17 @@
-<div class="hero-unit">
-  <h1>404</h1>
-  <blockquote>
-  	<p>Ever tried. Ever failed. No matter. Try Again. Fail again. Fail better.</p>
-  	<small>Samuel Beckett, <cite title="Source Title">Worstward Ho</cite></small>
-  </blockquote>
-</div>
+<style>
+body {
+background: url('img/404-02.jpg') no-repeat center center fixed;
+-webkit-background-size: cover;
+-moz-background-size: cover;
+-o-background-size: cover;
+background-size: cover;
+color: white;
+}
+</style>
+
+<h1>Page Not Found</h1>
+<blockquote>
+	<p>You can't always get what you want.</p>
+	<small>The Rolling Stones</small>
+</blockquote>
+<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
