11abd427f295adc426188272ded64fc3153f4fe8
Andrew Breakspear <andrew.breakspear@seh.ox.ac.uk>
2020-06-18 17:22:21 +0100

    both Set-AdUser commands now 1

--- nodes/reports/ad_mifare.php
+++ nodes/reports/ad_mifare.php
@@ -1,6 +1,6 @@
 <?php
 $personsClass = new Persons();
-	
+
 $personsAll = $personsClass->all();
 
 $studentArrayTypes = array('GT', 'GR', 'UG', 'VR', 'PT', 'VD', 'VV', 'VC');
@@ -17,7 +17,7 @@ foreach ($personsAll AS $person) {
 		//$output .= "<span>" . "test" . "</span>";
 		$studentOutput .= "<span class=\"asd\">" . $person['sso_username'] . "</span>";
 		$studentOutput .= "</a>";
-		
+
 		$studentOutputCount ++;
 	} else {
 		$otherOutput .= "<a class=\"mo od tc ra\" href=\"index.php?n=students_unique&cudid=" . $person['cudid'] . "\">";
@@ -25,7 +25,7 @@ foreach ($personsAll AS $person) {
 		//$output .= "<span>" . "test" . "</span>";
 		$otherOutput .= "<span class=\"asd\">" . $person['sso_username'] . "</span>";
 		$otherOutput .= "</a>";
-		
+
 		$otherOutputCount ++;
 	}
 }
@@ -34,17 +34,16 @@ foreach ($personsAll AS $person) {
 <code>
 	<?php
 	foreach ($personsAll AS $person) {
-		
+
 		if (isset($person['sso_username']) AND isset($person['oxford_email'])) {
 			$output  = "\$found= Get-ADUser -Filter \"SamAccountName -eq '" . $person['sso_username'] . "' -or mail -eq '" . $person['oxford_email'] . "'\" <br />";
 			$output .= "if(\$found){<br />";
-			
-			$output .= "\$found|Set-ADUser -Replace @{pager = '" . $person['MiFareID'] . "'} -Verbose <br />";
-			$output .= "\$found|Set-ADUser -Replace @{mail = '" . $person['oxford_email'] . "'} -Verbose <br />";
+
+			$output .= "\$found|Set-ADUser -Replace @{pager = '" . $person['MiFareID'] . "'; mail = '" . $person['oxford_email'] . "'} -Verbose <br />";
 			$output .= "}<br/><br/>";
-			
+
 			echo $output;
-			
+
 		} else {
 			// user is not a student
 		}
@@ -52,10 +51,10 @@ foreach ($personsAll AS $person) {
 			$output  = "Set-ADUser -Identity " . $person['sso_username'] . " ";
 		$output .= "-Replace @{pager ='" . $person['MiFareID'] . "'}";
 		$output .= "<br />";
-		
+
 		echo $output;
 		*/
 	}
-	
+
 	?>
 </code>
