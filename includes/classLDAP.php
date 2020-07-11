1c60f8b5bb97d6d6b527553ff12ec7919df99d32
Andrew Breakspear <andrew.breakspear@seh.ox.ac.uk>
2020-07-11 12:59:32 +0100

    randomPassword can have length set.  Defaults to 12

--- includes/classLDAP.php
+++ includes/classLDAP.php
@@ -204,12 +204,12 @@ class LDAP {
     return $users;
   }
 
-  public function randomPassword() {
+  public function randomPassword($length = 12) {
   	$alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
   	$pass = array(); //remember to declare $pass as an array
   	$alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
 
-  	for ($i = 0; $i < 12; $i++) {
+  	for ($i = 0; $i < $length; $i++) {
   		$n = rand(0, $alphaLength);
   		$pass[] = $alphabet[$n];
   	}
