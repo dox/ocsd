f9fbe902745a5ab82cb1e1edb99e6abc6216ba2f
Andrew Breakspear <andrew.breakspear@seh.ox.ac.uk>
2020-07-16 11:13:57 +0100

    strreplace now a function within Template class

--- includes/classTemplates.php
+++ includes/classTemplates.php
@@ -21,10 +21,24 @@ class Templates  {
 		$sql  = "SELECT * FROM " . self::$table_name;
     $sql .= " WHERE uid = '" . $uid . "'";
     $sql .= " OR name = '" . $uid . "'";
-    
+
 		$template = $db->query($sql)->fetchArray();
 
 		return $template;
 	}
+
+  public function oneBodyWithReplaceFields($name = null, $replaceFields = null) {
+    $template = $this->one($name);
+
+    if ($template) {
+      $messageBody = $template['body'];
+
+      foreach ($replaceFields AS $key => $value) {
+        $messageBody = str_replace("{{" . $key . "}}", $value, $messageBody);
+      }
+    }
+
+    return $messageBody;
+  }
 }
 ?>
