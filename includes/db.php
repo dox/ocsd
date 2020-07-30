573035fc93c6eff164dfd1e1c68770da152314b9
Andrew Breakspear <andrew.breakspear@seh.ox.ac.uk>
2020-07-30 08:55:37 +0100

    Removing old db connection class

--- includes/db.php
+++ includes/db.php
@@ -6,7 +6,7 @@ class db {
   protected $query_closed = TRUE;
 	public $query_count = 0;
 
-	public function __construct($dbhost = 'localhost', $dbuser = 'itsupport', $dbpass = '', $dbname = '', $charset = 'utf8') {
+	public function __construct($dbhost = 'localhost', $dbuser = '', $dbpass = '', $dbname = '', $charset = 'utf8') {
 		$this->connection = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 		if ($this->connection->connect_error) {
 			$this->error('Failed to connect to MySQL - ' . $this->connection->connect_error);
