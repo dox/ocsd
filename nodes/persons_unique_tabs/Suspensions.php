df5b3d6eb3aa349b74270e28c145f26f0b751a6a
Andrew Breakspear <andrew.breakspear@seh.ox.ac.uk>
2021-04-27 18:02:16 +0100

    Better logic for suspensions

--- nodes/persons_unique_tabs/Suspensions.php
+++ nodes/persons_unique_tabs/Suspensions.php
@@ -1,41 +1,18 @@
-<?php
+<?php if (!empty($person->getSuspensions())) { ?>
 
-$filename = basename(__FILE__, '.php');
-
-$sql  = "SELECT * FROM " . $filename;
-$sql .= " WHERE cudid = '" . $person->cudid . "'";
-$sql .= " ORDER BY SuspendSeq DESC";
-
-$dbOutput = $db->query($sql)->fetchAll();
-
-$sqlCurrent  = "SELECT * FROM " . $filename;
-$sqlCurrent .= " WHERE cudid = '" . $person->cudid . "'";
-$sqlCurrent .= " AND (DATE(SuspendStrDt) < '" . date('Y-m-d') . "'";
-$sqlCurrent .= " AND DATE(SuspendExpEndDt) > '" . date('Y-m-d') . "' AND SuspendEndDt IS null) OR (cudid = '" . $person->cudid . "' AND SuspendEndDt IS null)";
-
-$currentSuspension = $db->query($sqlCurrent)->fetchArray();
-?>
-
-<?php if ($dbOutput) { ?>
-
-<div class="card">
+<div class="card mb-3">
   <div class="card-header">
-    <h3 class="card-title"><?php echo $filename; ?></h3>
+    <h3 class="card-title">Suspensions</h3>
   </div>
   <div class="card-body">
     <?php
-    if ($currentSuspension) {
-      if (!empty($currentSuspension['SuspendEndDt'])) {
-        $suspensionEndDate = date('Y-m-d', strtotime($currentSuspension['SuspendEndDt']));
-      } else {
-        $suspensionEndDate = date('Y-m-d', strtotime($currentSuspension['SuspendExpEndDt']));
-      }
-      echo "<div class=\"alert alert-danger text-center\" role=\"alert\">CURRENTLY SUSPENDED UNTIL " . $suspensionEndDate . "</div>";
+    if ($person->isSuspended()) {
+      echo "<div class=\"alert alert-danger text-center\" role=\"alert\">CURRENTLY SUSPENDED</div>";
     }
     ?>
     <ul>
     <?php
-    foreach ($dbOutput AS $output) {
+    foreach ($person->getSuspensions() AS $output) {
       if (!isset($output['SuspendReason'])) {
         $output['SuspendReason'] = "Reason Unknown";
       }
