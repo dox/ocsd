ff0d8b16441f1e8a6790a1a2a47a897a14791c8a
Andrew Breakspear <andrew.breakspear@seh.ox.ac.uk>
2025-05-22 16:10:01 +0100

    iPlicit sync fuxes

--- cron/iplicit_sync.php
+++ cron/iplicit_sync.php
@@ -8,97 +8,137 @@ $i_students = 0;
 $iplicit = new iPlicitAPI();
 
 foreach ($cudPersons AS $cudPerson) {
-	$cudPerson = new Person($cudPerson['cudid']);
-	
+	// Regenerate API token if it's expired
 	$tokenTimeRemaining = strtotime($iplicit->tokenDue) - strtotime(gmdate('c'));
-	
 	if ($tokenTimeRemaining <= 1) {
 		cliOutput("Updating token, as it has expired", "green");
 		$iplicit->getSession();
 	}
-	if (isset($cudPerson->sits_student_code)) { // only perform lookup on CUD persons with a SITS code
-		$i_students++; // count how many students we're processing
-
-		$exisitingiplicitContact = $iplicit->getContactAccount($cudPerson->sits_student_code);
-		$iPlicitFriendlyCUDArray = $iplicit->cudidToiPlicitContact($cudPerson->cudid);
-		
-		if (isset($exisitingiplicitContact->id)) { // contact already exists in iPlicit
-			//only update if something is different
-			if ($iplicit->updateRequired($iPlicitFriendlyCUDArray, $exisitingiplicitContact)){
-				$iplicit->updateContactAccount($exisitingiplicitContact->code, $iPlicitFriendlyCUDArray);
-			} else {
-				cliOutput("Skipping update for " . $cudPerson->FullName . " (" . $cudPerson->sits_student_code . ")", "white");
-			}
-			
-		} else { // contact needs to be crated in iPlicit
-			// add additional fields
-			$iPlicitFriendlyCUDArray['customer']['paymentMethodId'] = "BC";
-			
-			$iplicit->createContactAccount($iPlicitFriendlyCUDArray);
-		}
-	}
-}
-
-cliOutput($i_students . " students processed of " . count($cudPersons) . " CUD persons", "green");
-cliOutput($iplicit->i_updated . " students updated", "green");
-cliOutput($iplicit->i_created . " students created", "green");
-cliOutput($iplicit->i_error . " errors encountered", "red");
-
-# ----------------------------------------------- #
-#           PROCESS EMAIL NOTIFICATION            #
-# ----------------------------------------------- #
-//email here!
-$mail_body  = "<p>iPlicit/CUD sync complete for " . $i_students . autoPluralise(" user ", " users ", $i_students) . "with SITS IDs (of a total of " . count($cudPersons) . " CUD persons) at " . date('Y-m-d H:i:s') . "</p>";
-$mail_body .= "<p>" . $iplicit->i_updated . autoPluralise(" account was ", " accounts were ", $iplicit->i_updated) . "updated.</p>";
-$mail_body .= "<p>The following " . $iplicit->i_created . autoPluralise(" account was ", " accounts were ", $i_created) . "created:</p>";
-$mail_body .= "<ul>";
-
-foreach ($iplicit->createLog AS $transaction) {
-  $mail_body .= "<li>" . $transaction . "</li>";
-}
-$mail_body .= "</ul>";
-
-if ($iplicit->i_error > 0) {
-	$mail_body .= "<hr />";
-	$mail_body .= "<p>The following " . $iplicit->i_error . autoPluralise(" error was ", " errors were ", $iplicit->i_error) . "encountered:</p>";
-	$mail_body .= "<ul>";
 	
-	foreach ($iplicit->errorLog AS $transaction) {
-	  $mail_body .= "<li>" . $transaction . "</li>";
+	
+	$cudPerson = new Person($cudPerson['cudid']);
+	
+	if (!isset($cudPerson->sits_student_code)) {
+		// Missing student ID, so skip this record
+		continue;
 	}
-	$mail_body .= "</ul>";
-	$mail_body .= "<hr />";
+	
+	$i_students++; // count how many students we're processing
+	
+	$address = $cudPerson->addresses()->getHomeAddress();
+	
+	$addressParts = [
+		$address['Line1'] ?? null,
+		$address['Line2'] ?? null,
+		$address['Line3'] ?? null,
+		$address['Line4'] ?? null,
+		$address['Line5'] ?? null,
+		$address['City'] ?? null,
+		$address['State'] ?? null,
+		$address['County'] ?? null
+	];
+	
+	// Filter out empty/null values & Join into a single line
+	$filtered = array_filter($addressParts, fn($part) => !empty($part));
+	$singleLineAddress = implode(', ', $filtered);
+	
+	$cudData = [
+		 'code' => $cudPerson->sits_student_code,
+		 'description' => $cudPerson->FullName,
+		 'customer' => [
+			 'ext' => [
+				 'Activestatus' => $cudPerson->Enrolments()->all()[0]['SCJStatusName'],
+				 'SSO' => $cudPerson->sso_username,
+				 'Currentyear' => $cudPerson->unit_set_cd,
+			 ],
+		 ],
+		 'contact' => [
+			 'intRef' => $cudPerson->sits_student_code,
+			 'companyName' => $cudPerson->FullName,
+			 'title' => $cudPerson->titl_cd,
+			 'firstName' => $cudPerson->firstname,
+			 'lastName' => $cudPerson->lastname
+		 ]
+	];
+	
+	# Phones
+	if (isset($cudPerson->addresses()->getContactAddress()['TelNo'])) {
+		$cudData['contact']['phones'][] = [
+			'type' => 'H',
+			'phone' => $cudPerson->addresses()->getContactAddress()['TelNo'],
+		];
+	}
+	if (isset($cudPerson->addresses()->getContactAddress()['MobileNo'])) {
+		$cudData['contact']['phones'][] = [
+			'type' => 'M',
+			'phone' => $cudPerson->addresses()->getContactAddress()['MobileNo'],
+		];
+	}
+	
+	#Emails
+	if (isset($cudPerson->oxford_email)) {
+		$cudData['contact']['emails'][] = [
+			'type' => 'R',
+			'email' => $cudPerson->oxford_email,
+		];
+	}
+	if (isset($cudPerson->alt_email)) {
+		$cudData['contact']['emails'][] = [
+			'type' => 'P',
+			'email' => $cudPerson->alt_email,
+		];
+	}
+	
+	#Addressess
+	if (isset($singleLineAddress)) {
+		$cudData['contact']['addresses'][] = [
+			'type' => 'R',
+			'address' =>  transliterator_transliterate('Any-Latin; Latin-ASCII; [\u0100-\u7fff] remove', $singleLineAddress),
+			'postCode' => $address['PostCode'],
+			'city' => $address['County'],
+			'countryCode' => cudCountryCodeToiPlicitCountyCode($address['AddressCtyCd'])
+		];
+	}
+	
+	$iplicitData = $iplicit->getContactAccount($cudPerson->sits_student_code);
+	if (isset($iplicitData->id)) {
+		// account exists, update it
+		
+		if (!isset($cudData['contact']['addresses'][0]['address'])) {
+			unset($cudData['contact']['addresses'][0]);
+		}
+		
+		//printArray($cudData);
+		cliOutput("Updating " . $cudPerson->FullName . " (" . $iplicitData->id . ")", "green");
+		$iplicit->updateContactAccount($iplicitData->id, $cudData);
+			
+	} else {
+		// account needs creating
+		$iplicit->createContactAccount($cudData);
+	}
+	
 }
 
-$mail_subject = "iPlicit/CUD sync";
-$mail_recipients = iplicit_api_notifications;
-
-// only email if accounts were created
-if ($iplicit->i_created > 0 || $iplicit->i_error > 0) {
-  sendMail($mail_subject, $mail_recipients, $mail_body);
-  cliOutput("Sending email to: " . implode(", ", $mail_recipients), "green");
+cliOutput(count($iplicit->updateLog) . " students updated of " .  " CUD persons", "green");
+cliOutput(count($iplicit->createLog) . " students created", "green");
+if (count($iplicit->errorLog) > 0) {
+	cliOutput(count($iplicit->errorLog) . " errors encountered", "red");
+} else {
+	cliOutput(count($iplicit->errorLog) . " errors encountered", "green");
 }
 
-$logInsert = (new Logs)->insert("cron","success",null,"iPlicit sync complete for " . $i_students . " student(s)");
 
-
-
-
-
-
-
+if (count($iplicit->errorLog) > 0) {
+	sendMail('iPlicit sync errors', 'andrew.breakspear@seh.ox.ac.uk', null, implode(', ', $iPlicit->errorLog));
+}
 
 class iPlicitAPI {
 	public $sessionToken;
 	public $tokenDue;
-	
-	public $i_updated = 0; 	// total iPlicit contacts updated
-	public $i_created = 0; 	// total iPlicit contacts created
-	public $i_error = 0; 	// total iPlicit errors encountered
 
-	public $updateLog = array(); 	// array of iPlicit updates
-	public $createLog = array(); 	// array of iPlicit creations
-	public $errorLog = array(); 	// array of iPlicit errors
+	public array $updateLog = []; 	// array of iPlicit updates
+	public array $createLog = []; 	// array of iPlicit creations
+	public array $errorLog = []; 	// array of iPlicit errors
 	
 	function __construct() {
 		$this->getSession();
@@ -165,6 +205,8 @@ class iPlicitAPI {
 	}
 	
 	public function updateContactAccount($idOrCode, $contactArray) {
+		global $log;
+		
 		$url = "https://api.iplicit.com/api/ContactAccount/" . $idOrCode;
 		
 		$curl = curl_init();
@@ -175,29 +217,13 @@ class iPlicitAPI {
 		curl_setopt($curl, CURLOPT_HTTPHEADER, $this->headers());
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 		
-		$data = json_decode(curl_exec($curl));
-		
-		if (isset($data->type) || isset($data->message)) {
-			$this->i_error++;
-			
+		try {
+			$data = json_decode(curl_exec($curl));
+			$this->updateLog[] = $idOrCode;
+		} catch(Exception $e) {
 			$event = "Error updating iPlicit record for " . $contactArray['description'] . " (" . $contactArray['code'] . ") - " . json_encode($data);
-			
 			$this->errorLog[] = $event;
 			cliOutput($event, "red");
-			debug($event);
-			$logInsert = (new Logs)->insert("cron","error",null,$event);
-		} else {
-			$this->i_updated++;
-			
-			$event = "Updated iPlicit record for " . $contactArray['description'] . " (" . $contactArray['code'] . ") - " . json_encode($data);
-			
-			$this->updateLog[] = $event;
-			cliOutput($event, "green");
-			debug($event);
-			
-			if (debug) {
-				$logInsert = (new Logs)->insert("cron","success",null,$event);
-			}
 		}
 		
 		curl_close($curl);
@@ -214,205 +240,16 @@ class iPlicitAPI {
 		curl_setopt($curl, CURLOPT_HTTPHEADER, $this->headers());
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 		
-		$data = json_decode(curl_exec($curl));
-		
-		if (isset($data->type) || isset($data->message)) {
-			$this->i_error++;
-			
+		try {
+			$data = json_decode(curl_exec($curl));
+			$this->createLog[] = "complete";
+		} catch(Exception $e) {
 			$event = "Error creating iPlicit record for " . $contactArray['description'] . " (" . $contactArray['code'] . ") - " . json_encode($data);
-			
 			$this->errorLog[] = $event;
 			cliOutput($event, "red");
-			debug($event);
-			$logInsert = (new Logs)->insert("cron","error",null,$event);
-		} else {
-			$this->i_created++;
-			
-			$event = "Created iPlicit record for " . $contactArray['description'] . " (" . $contactArray['code'] . ") - " . json_encode($data);
-			
-			$this->createLog[] = $event;
-			cliOutput($event, "green");
-			debug($event);
-			$logInsert = (new Logs)->insert("cron","success",null,$event);
 		}
 		
 		curl_close($curl);
-	}
-	
-	public function cudidToiPlicitContact($cudid) {
-		// find a person from their cudid and return an array formatted corrected for iPlicit
-		
-		global $db;
-		
-		$cudPerson = new Person($cudid);
-		
-		$sql  = "SELECT * FROM Enrolments WHERE cudid = '" . $cudPerson->cudid . "' ORDER BY SCJSequence DESC";
-		$cudPersonEnrolments = $db->get($sql);
-		
-		$sql2  = "SELECT * FROM EnrolAwdProg WHERE cudid = '" . $cudPerson->cudid . "' ORDER BY Code DESC";
-		$cudPersonEnrolAwdProg = $db->get($sql);
-		
-		$iplicitContact['description'] = $cudPerson->FullName;
-		$iplicitContact['code'] = $cudPerson->sits_student_code;
-		$iplicitContact['contact']['intRef'] = $cudPerson->sits_student_code;
-		$iplicitContact['contact']['title'] = $cudPerson->titl_cd;
-		$iplicitContact['contact']['firstName'] = $cudPerson->firstname;
-		$iplicitContact['contact']['middleName'] = $cudPerson->middlenames;
-		$iplicitContact['contact']['lastName'] = $cudPerson->lastname;
-		$iplicitContact['customer']['ext']['Currentyear'] = $cudPerson->unit_set_cd;
-		
-		$iplicitContact['customer']['ext']['AwardProgrammeTitle'] = $cudPersonEnrolAwdProg['AwdName'];
-		$iplicitContact['customer']['ext']['AwardProgrammeCode'] = $cudPersonEnrolAwdProg['CrsCd'];
-		$iplicitContact['customer']['ext']['ExpectedEndDate'] = $cudPersonEnrolments['CrsExpEndDt'];
-
-		if (isset($cudPerson->oxford_email)) {
-		  $iplicitContact['contact']['emails'][] = array("type" => "R", "email" => $cudPerson->oxford_email);
-		}
-		if (isset($cudPerson->alt_email)) {
-		  $iplicitContact['contact']['emails'][] = array("type" => "P", "email" => $cudPerson->alt_email);
-		}
-		
-		$cudAddress = $cudPerson->addresses()->getContactAddress();
-		
-		$cleanAddress = null;
-		if (isset($cudAddress['AddressCtryCd']) || isset($cudAddress['Line1']) || isset($cudAddress['Line2'])) {
-		  if (isset($cudAddress['Line1'])) {
-			$cleanAddress = $cudAddress['Line1'];
-		  }
-		  if (isset($cudAddress['Line2'])) {
-			$cleanAddress = $cleanAddress . ", " . $cudAddress['Line2'];
-		  }
-		  if (isset($cudAddress['Line3'])) {
-			$cleanAddress = $cleanAddress . ", " . $cudAddress['Line3'];
-		  }
-		  if (isset($cudAddress['Line4'])) {
-			$cleanAddress = $cleanAddress . ", " . $cudAddress['Line4'];
-		  }
-		  if (isset($cudAddress['Line5'])) {
-			$cleanAddress = $cleanAddress . ", " . $cudAddress['Line5'];
-		  }
-		  
-		  if (!empty($cudAddress['PostCode'])) {
-			  $cleanPostcode = $cudAddress['PostCode'];
-		  } else {
-			  $cleanPostcode = "Unknown";
-		  }
-		  
-		  $iplicitContact['contact']['addresses'][0]['type'] = "R";
-		  $iplicitContact['contact']['addresses'][0]['address'] = $cleanAddress;
-		  $iplicitContact['contact']['addresses'][0]['postcode'] = $cleanPostcode;
-		  $iplicitContact['contact']['addresses'][0]['city'] = $cudAddress['City'] . " " . $cudAddress['State'];
-		  $iplicitContact['contact']['addresses'][0]['county'] = $cudAddress['County'];
-		  $iplicitContact['contact']['addresses'][0]['countryCode'] = cudCountryCodeToiPlicitCountyCode($cudAddress['AddressCtryCd']);
-		  $iplicitContact['contact']['addresses'][0]['description'] = "Last updated: " . $cudAddress['LastUpdateDt'];
-		}
-		
-		if (!empty($cudAddress['TelNo'])) {
-		  $iplicitContact['contact']['phones'][] = array("type" => "H", 'phone' => $cudAddress['TelNo']);
-		}
-		if (!empty($cudAddress['MobileNo'])) {
-		  $iplicitContact['contact']['phones'][] = array("type" => "M", 'phone' => $cudAddress['MobileNo']);
-		}
-		
-		if (!empty($cudPerson->sso_username)) {
-		  $iplicitContact['customer']['ext']['SSO'] = $cudPerson->sso_username;
-		}
-		
-		if (!empty($cudPersonEnrolments['SCJStatusName'])) {
-		  $iplicitContact['customer']['ext']['Activestatus'] = $cudPersonEnrolments['SCJStatusName'];
-		}
-		
-		$iplicitContact['customer']['contactGroupCustomerId'] = cudCardTypeToiPlicitGroup($cudPerson->university_card_type);
-		
-		return $iplicitContact;
-	}
-	
-	public function updateRequired($cud, $iplicit) {
-		$update = false;
-		$changeFields = array();
-		
-		
-		
-		if ($cud['description'] != $iplicit->description) {
-			$changeFields['description'] = $cud['description'] . " != " . $iplicit->description;
-			$update = true;
-		}
-		if ($cud['contact']['title'] != $iplicit->contact->title) {
-			$update = true;
-			$changeFields['title'] = $cud['contact']['title'] . " != " . $iplicit->contact->title;
-		}
-		if ($cud['contact']['firstName'] != $iplicit->contact->firstName) {
-			$update = true;
-			$changeFields['firstName'] = $cud['contact']['firstName'] . " != " . $iplicit->contact->firstName;
-		}
-		if ($cud['contact']['middleName'] != $iplicit->contact->middleName) {
-			$update = true;
-			$changeFields['middleName'] = $cud['contact']['middleName'] . " != " . $iplicit->contact->middleName;
-		}
-		if ($cud['contact']['lastName'] != $iplicit->contact->lastName) {
-			$update = true;
-			$changeFields['lastName'] = $cud['contact']['lastName'] . " != " . $iplicit->contact->lastName;
-		}
-		
-		if ($cud['contact']['emails'][0]['email'] != $iplicit->contact->emails[0]->email) {
-			$update = true;
-			$changeFields['email0'] = $cud['contact']['emails'][0]['email'] . " != " . $iplicit->contact->emails[0]->email;
-		}
-		if ($cud['contact']['emails'][1]['email'] != $iplicit->contact->emails[1]->email) {
-			$update = true;
-			$changeFields['email1'] = $cud['contact']['emails'][1]['email'] . " != " . $iplicit->contact->emails[1]->email;
-		}
-		
-		if ($cud['contact']['phones'][0]['phone'] != $iplicit->contact->phones[0]->phone) {
-			$update = true;
-			$changeFields['phone0'] = $cud['contact']['phones'][0]['phone'] . " != " . $iplicit->contact->phones[0]->phone;
-		}
-		if ($cud['contact']['phones'][1]['phone'] != $iplicit->contact->phones[1]->phone) {
-			$update = true;
-			$changeFields['phone1'] = $cud['contact']['phones'][1]['phone'] . " != " . $iplicit->contact->phones[1]->phone;
-		}
-		
-		if ($cud['customer']['ext']['SSO'] != $iplicit->customer->ext->SSO) {
-			$update = true;
-			$changeFields['email1'] = $cud['contact']['ext']['sso'] . " != " . $iplicit->contact->ext->sso;
-		}
-		
-		if ($cud['customer']['ext']['Activestatus'] != $iplicit->customer->ext->Activestatus) {
-			$update = true;
-			$changeFields['Activestatus'] = $cud['contact']['ext']['Activestatus'] . " != " . $iplicit->contact->ext->Activestatus;
-		}
-		
-		if ($cud['customer']['ext']['Currentyear'] != $iplicit->customer->ext->Currentyear) {
-			$update = true;
-			$changeFields['Currentyear'] = $cud['contact']['ext']['Currentyear'] . " != " . $iplicit->contact->ext->Currentyear;
-		}
-		
-		if ($cud['customer']['contactGroupCustomerId'] != $iplicit->customer->contactGroupCustomerId) {
-			//$update = true;
-			//$changeFields['contactGroupCustomerId'] = $cud['customer']['contactGroupCustomerId'] . " != " . $iplicit->customer->contactGroupCustomerId;
-		}
-		
-		if ($cud['customer']['ext']['AwardProgrammeTitle'] != $iplicit->customer->ext->AwardProgrammeTitle ||
-			$cud['customer']['ext']['AwardProgrammeCode'] != $iplicit->customer->ext->AwardProgrammeCode ||
-			$cud['customer']['ext']['ExpectedEndDate'] != $iplicit->customer->ext->ExpectedEndDate) {
-			
-			$update = true;
-			$changeFields['AwardProgrammeTitle'] = $cud['customer']['ext']['AwardProgrammeTitle'];
-			$changeFields['AwardProgrammeCode'] = $cud['customer']['ext']['AwardProgrammeCode'];
-			$changeFields['ExpectedEndDate'] = $cud['customer']['ext']['ExpectedEndDate'];
-		}
-
-
-		if ($cud['contact']['addresses'][0]['address'] != $iplicit->contact->addresses[0]->address) {
-			$update = true;
-			$changeFields['address'] = $cud['contact']['addresses'][0]['address'] . " != " . $iplicit->contact->addresses[0]->address;
-		}
-		
-		if (!empty($changeFields)){
-			printArray($changeFields);
-		}
-		
-		return $update;
 	}
 }
 
@@ -685,4 +522,3 @@ function cudCardTypeToiPlicitGroup($cudCardType) {
   
   return $iPlicitGroup;
 }
-?>
